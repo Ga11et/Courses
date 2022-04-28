7e846f0a4076c4f09b3a1f8caa4e766bb6bb307b
Fyodor <sivkov1998@yandex.ru>
2022-04-28 16:16:33 +0300

    added some hovers to header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react"
+import { Link, useNavigate } from "react-router-dom"
 import { Dropdown } from "../../common/dropdown/dropdown"
 import { MobileDropdown } from "../mobileDropdown/mobileDropdown"
 import css from './header.module.css'
@@ -8,15 +9,17 @@ type HeaderPropsType = {
 }
 export const Header: FC<HeaderPropsType> = ({  }) => {
 
+    const navigate = useNavigate()
+
     return <>
         <header className={css.header}>
-            <div className={css.logo}>
+            <div onClick={() => navigate('./findcourse')} className={css.logo}>
                 Cursator
             </div>
             <ol className={css.menu}>
                 <li className={css.menu__item}><Dropdown insideText="Все курсы" /></li>
                 <li className={css.menu__item}><a href="/">Тест на профессию</a></li>
-                <li className={css.menu__item}><a href="/">Акции</a></li>
+                <li className={css.menu__item}><Link to={'./stocks'}>Акции</Link></li>
                 <li className={css.menu__item}><a href="/">Отзывы</a></li>
             </ol>
             <div className={css.mobileMenu}>
